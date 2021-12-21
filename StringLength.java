f58ae2de4db23a61eec724e94d0406eaf8aa162b
Kruthika R <65350577+Kruthika-ML@users.noreply.github.com>
2021-12-21 11:06:21 +0530

    Add files via upload

---
+++ StringLength.java
@@ -0,0 +1,6 @@
+public class StringLength {
+    public static void main(String[] args) {
+        String txt = "Kruthika";
+        System.out.println("The length of the string is " +txt.length());
+    }
+}
